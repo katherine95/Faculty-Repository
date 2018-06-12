fc977e5df77324e227b574bfb664921b3e38a9d1
Juma Allan <allanjuma@gmail.com>
2018-06-12 20:39:54 +0300

    Added Menu Resource

--- app/src/main/java/com/example/facultyapp/ui/main/ui/MainActivity.java
+++ app/src/main/java/com/example/facultyapp/ui/main/ui/MainActivity.java
@@ -1,15 +1,138 @@
 package com.example.facultyapp.ui.main.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.facultyapp.R;
+import com.example.facultyapp.settings.Settings;
+import com.example.facultyapp.ui.auth.ui.AuthActivity;
+import com.example.facultyapp.ui.main.viewmodel.MainViewModel;
+import com.example.facultyapp.view.ProfileDialog;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.tingyik90.snackprogressbar.SnackProgressBar;
+import com.tingyik90.snackprogressbar.SnackProgressBarManager;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+
+import static com.example.facultyapp.BR.user;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private ProfileDialog profileDialog;
+    private SnackProgressBarManager snackProgressBarManager;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainViewModel mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        user = mainViewModel.getUserLiveData();
+        binding.setUser(user);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In Api and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem profileItem = menu.findItem(R.id.action_profile);
+        Glide.with(this)
+                .asBitmap()
+                .load(user.getImageUrl())
+                .apply(RequestOptions.circleCropTransform())
+                .into(new SimpleTarget<Bitmap>(100, 100) {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+                        profileItem.setIcon(new BitmapDrawable(getResources(), resource));
+                    }
+                });
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_profile) {
+            profileDialog.show(getSupportFragmentManager(), "profile");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        if (!Settings.isLoggedIn()) {
+            return;
+        }
+
+        SnackProgressBar snackProgressBar = new SnackProgressBar(
+                SnackProgressBar.TYPE_INDETERMINATE,
+                "Logging Out...")
+                .setSwipeToDismiss(false);
+
+        // Show snack progress during logout
+        snackProgressBarManager.dismissAll();
+        snackProgressBarManager.show(snackProgressBar, SnackProgressBarManager.LENGTH_INDEFINITE);
+
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(status -> {
+            //Clear Shared Pref File
+            Settings.setLoggedInSharedPref(false);
+            //Clear Local DB
+            //TODO :: CLEAR DB
+
+            //Redirect User to Login Page
+            Intent intent = new Intent(getApplicationContext(), AuthActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        //Unreachable anyway
+        snackProgressBarManager.dismiss();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
