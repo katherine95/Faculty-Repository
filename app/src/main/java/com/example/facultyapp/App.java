599b2555407a641f10e37e18c1fb89fb513d2534
Juma Allan <allanjuma@gmail.com>
2018-06-12 20:01:58 +0300

    Added Custom Fonts

--- app/src/main/java/com/example/facultyapp/App.java
+++ app/src/main/java/com/example/facultyapp/App.java
@@ -1,6 +1,57 @@
 package com.example.facultyapp;
 
 import android.app.Application;
+import android.graphics.Typeface;
+
+import com.example.facultyapp.settings.Settings;
+import com.example.facultyapp.util.TypeFactory;
+
+import timber.log.Timber;
 
 public class App extends Application {
+    private static App mInstance;
+    private TypeFactory mFontFactory;
+
+    public static synchronized App getApp() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Plant a Debug Timber Tree to log :)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        // init Settings
+        Settings.init(this);
+        mInstance = this;
+    }
+
+    public Typeface getTypeFace(int type) {
+        if (mFontFactory == null)
+            mFontFactory = new TypeFactory(this);
+
+        switch (type) {
+            case Constants.REGULAR:
+                return mFontFactory.getRegular();
+
+            case Constants.BOLD:
+                return mFontFactory.getBold();
+
+            case Constants.LIGHT:
+                return mFontFactory.getLight();
+
+            default:
+                return mFontFactory.getRegular();
+        }
+    }
+
+    public interface Constants {
+        int REGULAR = 1,
+                BOLD = 2,
+                LIGHT = 3;
+    }
 }
