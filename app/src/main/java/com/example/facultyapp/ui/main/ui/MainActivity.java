c9df710986995bf2a68b0af889c66229e627e8f3
Juma Allan <allanjuma@gmail.com>
2018-06-12 19:27:42 +0300

    Added Utils

--- app/src/main/java/com/example/facultyapp/ui/main/ui/MainActivity.java
+++ app/src/main/java/com/example/facultyapp/ui/main/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.facultyapp;
+package com.example.facultyapp.ui.main.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.facultyapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
