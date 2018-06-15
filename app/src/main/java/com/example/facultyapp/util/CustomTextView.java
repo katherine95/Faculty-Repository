7e0bbabdec3652921c33123e87779a37438d8463
Juma Allan <allanjuma@gmail.com>
2018-06-15 23:48:23 +0300

    Added student card

---
+++ app/src/main/java/com/example/facultyapp/util/CustomTextView.java
@@ -0,0 +1,30 @@
+package com.example.facultyapp.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView {
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/oxygen_bold.ttf");
+            setTypeface(tf);
+        }
+    }
+}
